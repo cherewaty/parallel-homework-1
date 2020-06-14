aaac787844c3af55765fea04894f24fa6c3c354a
Jeffrey Cherewaty <jeffrey@frontside.com>
2020-06-14 17:02:48 -0500

    Implement non-parallel version of Euclidean distance

--- src/hw1.cpp
+++ src/hw1.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 #include <omp.h>
+#include <cmath>
 #include "hw1.h"
 
 double euclidean_length(std::vector<double> vector)
 {
   // Your code goes here.
-  return 0;
+
+  // non-parallel version
+  // http://polaris.s.kanazawa-u.ac.jp/~npozar/basic-cpp-for-numerics-vectors.html
+  double accum = 0.;
+  for (int i = 0; i < vector.size(); ++i)
+  {
+    accum += vector[i] * vector[i];
+  }
+  return sqrt(accum);
 }
+
 std::vector<int64_t> discard_duplicates(std::vector<int64_t> sorted_vector)
 {
   // Your code goes here
