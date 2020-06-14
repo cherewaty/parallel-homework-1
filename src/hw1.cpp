579b66d77e98c112d9f16b72cb8f171a972c9312
Jeffrey Cherewaty <jeffrey@frontside.com>
2020-06-14 17:38:26 -0500

    Implement problem 6, non-parallel

--- src/hw1.cpp
+++ src/hw1.cpp
@@ -5,20 +5,33 @@
 
 double euclidean_length(std::vector<double> vector)
 {
-  // Your code goes here.
-
-  // non-parallel version
+  // non-parallel reference at
   // http://polaris.s.kanazawa-u.ac.jp/~npozar/basic-cpp-for-numerics-vectors.html
-  double accum = 0.;
-  for (int i = 0; i < vector.size(); ++i)
+
+  double sum = 0.0;
+  int i;
+
+#pragma omp parallel for reduction(+ \
+                                   : sum)
+  for (i = 0; i < vector.size(); ++i)
   {
-    accum += vector[i] * vector[i];
+    sum += vector[i] * vector[i];
   }
-  return sqrt(accum);
+  return sqrt(sum);
 }
 
 std::vector<int64_t> discard_duplicates(std::vector<int64_t> sorted_vector)
 {
-  // Your code goes here
-  return std::vector<int64_t>();
+  std::vector<int64_t> unduplicated = {};
+  int i;
+
+  for (i = 0; i < sorted_vector.size(); ++i)
+  {
+    if (i == 0 || sorted_vector[i] != sorted_vector[i - 1])
+    {
+      unduplicated.push_back(sorted_vector[i]);
+    }
+  }
+
+  return unduplicated;
 }
