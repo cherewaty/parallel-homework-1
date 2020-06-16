8fd2555f379fb10542d61bb39709b15f312b00cb
abigailfrancis <abigailfrancis@email.arizona.edu>
2020-06-16 02:07:36 -0400

    implement duplicate arrays

--- src/hw1.cpp
+++ src/hw1.cpp
@@ -5,33 +5,43 @@
 
 double euclidean_length(std::vector<double> vector)
 {
-  int i = 0;
-  int d = 0;
-	omp_set_num_threads(vector.size());
-	#pragma omp parallel\
-	  private(i) default(shared)
-	i = omp_get_thread_num();
-  	vector[i] = vector[i] * vector[i];
-	#pragma omp barrier
-	//	#pragma omp single
-	for (d = 1; d<vector.size(); d = 2*d){
-		omp_set_num_threads(vector.size());
-		#pragma omp parallel default(shared) private(i)
-		i = omp_get_thread_num();
-		if (i>0 && i<(vector.size()-1)){
-				if(i-d>0){
-					vector[i] = vector[i] + vector[i-d];
-				}
-			}
-		#pragma omp barrier
-	}
-  	//now we have a sum in the vector[i]
+  int i=0;
+  omp_set_num_threads(vector.size());
+
+#pragma omp parallel for\
+  private(i)
+
+  for(i = 0; i<vector.size(); i++){
+    vector[i] = vector[i]*vector[i];
+  }
+  #pragma omp barrier
+
+  double sum = 0.0;
+  #pragma omp parallel for reduction (+:sum)
+  for(int i=0; i<(vector.size()); i++){
+      sum = sum + vector[i];
+  }
   	
-  return sqrt(vector[vector.size()-1]);
+  return sqrt(sum);
 }
 std::vector<int64_t> discard_duplicates(std::vector<int64_t> sorted_vector)
 {
-  //in parallel 
+  //in parallel
+  int i;
+  std::vector<int64_t>B;
+  
+#pragma omp parallel for ordered  private(i) 
+  for (i = 0; i < sorted_vector.size(); i++){
+    if(i == 0 || sorted_vector[i] >  sorted_vector[i-1] ){
+#pragma omp ordered
+      B.push_back(sorted_vector[i]);
+    }
+  }
+  
+  if(sorted_vector[sorted_vector.size()-1] != sorted_vector[sorted_vector.size()-1]){
+    B.push_back(sorted_vector[sorted_vector.size()-1]);
+  }
   // Your code goes here
-  return std::vector<int64_t>();
+    //  return std::vector<int64_t>(B);
+    return B;
 }
