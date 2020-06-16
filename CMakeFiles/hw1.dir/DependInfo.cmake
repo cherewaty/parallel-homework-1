c6d55aaa9354591e99d301dcd50fc8cd5fbc3a59
abigailfrancis <abigailfrancis@email.arizona.edu>
2020-06-15 20:55:37 -0400

    euclidean length initial function. OMP functionality not working correctly, blocks are not executed

---
+++ CMakeFiles/hw1.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/abigail/Documents/parallelAlg/HW1/parallel-homework-1/src/hw1.cpp" "/home/abigail/Documents/parallelAlg/HW1/CMakeFiles/hw1.dir/src/hw1.cpp.o"
+  "/home/abigail/Documents/parallelAlg/HW1/parallel-homework-1/src/main.cpp" "/home/abigail/Documents/parallelAlg/HW1/CMakeFiles/hw1.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
